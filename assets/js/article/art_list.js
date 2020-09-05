3b9bc123f02d3a9acb17cd118c461ac33ad2dcf4
wrr <1973976518@qq.com>
2020-09-05 21:01:58 +0800

    完成了文章管理的功能开发

---
+++ assets/js/article/art_list.js
@@ -0,0 +1,136 @@
+$(function () {
+    var layer = layui.layer
+    var form = layui.form
+    var laypage = layui.laypage
+    // 定义美化时间的过滤器
+    template.defaults.imports.dataFormat = function (date) {
+        const dt = new Date(date)
+
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+
+        return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+    }
+
+    // 定义补零的函数
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
+    // 定义一个查询的参数对象，将来请求数据的时候，
+    // 需要将请求参数对象提交到服务器
+    var q = {
+        pagenum: 1, // 页码值，默认请求第一页的数据
+        pagesize: 2, // 每页显示几条数据，默认每页显示2条
+        cate_id: '', // 文章分类的 Id
+        state: '' // 文章的发布状态
+    }
+    initTable()
+
+    // 获取文章列表数据的方法
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章列表失败！')
+                }
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+                //调用渲染分页的方法
+                renderPage(res.total)
+            }
+        })
+    }
+
+    initCate()
+
+    // 初始化文章分类的方法
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取分类数据失败！')
+                }
+                // 调用模板引擎渲染分类的可选项
+                var htmlStr = template('tpl-cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                // 通过 layui 重新渲染表单区域的UI结构
+                form.render()
+            }
+        })
+    }
+    //为查询参数对象q中对应的属性赋值
+    $('#form-search').on('submit', function (e) {
+        e.preventDefault()
+        // 获取表单中选中项的值
+        var cate_id = $('[name=cate_id]').val()
+        var state = $('[name=state]').val()
+        // 为查询参数对象 q 中对应的属性赋值
+        q.cate_id = cate_id
+        q.state = state
+        // 根据最新的筛选条件，重新渲染表格的数据
+        initTable()
+    })
+
+    //定义渲染分页的方法，接收一个总数量的参数
+    function renderPage(total) {
+        laypage.render({
+            elem: 'pageBox',//分页容器的id
+            count: total,//总数据条数
+            limit: q.pagesize,//每页显示几条数据
+            curr: q.pagenum,//设置默认被选中的分页
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits: [1, 2, 3, 5, 10],
+            //分页发生切换的时候，触发jump回调
+            jump: function (obj, first) {
+                console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
+                console.log(obj.limit); //得到每页显示的条数
+                //把最新的页码值，赋值给q这个查询参数
+                q.pagenum = obj.curr
+                //把最新的条目数，赋值给q这个查询参数pagesize属性中
+                q.pagesize = obj.limit
+                //根据最新的q获取对饮的数据列表， 并渲染表格
+                if (!first) {
+                    initTable()
+                }
+            }
+
+        })
+    }
+    // 通过代理的形式，为 btn-delete表单绑定 click 事件
+    $('tbody').on('click', '.btn-delete', function () {
+        //获取删除按钮个数
+        var len = $('.btn-delete').length
+        //获取文章的id
+        var id = $(this).attr('data-id')
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function (index) {
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/delete/' + id,
+                success: function (res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除文章失败！')
+                    }
+                    layer.msg('删除文章成功！')
+                    if (len === 1) {
+                        //页码值最小必须是1
+                        q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
+                    }
+                    initTable()
+                }
+            })
+            layer.close(index);
+        })
+    })
+
+
+})
